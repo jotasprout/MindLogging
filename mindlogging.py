cd08c7bdb62647cd124576e4e82e76aa02394366
jotasprout <jotasprout@gmail.com>
2019-04-09 10:34:09 -0400

    Write pseudo-code

---
+++ mindlogging.py
@@ -0,0 +1,29 @@
+# Launch Chrome (maybe need to use Automator -- and then have that Python Launcher launch this python script)
+# Open employee portal URL
+
+# In employer authentication page
+# Enter email
+# Enter username (Be all fancy by using tuple for sidebar)
+# Enter password
+# Click "Sign In"
+
+# On employee portal home page
+# Click pseudo-'LMS'-ish icon
+
+# On citrix authentication page
+# Enter username
+# Enter pw
+# Click "Log On"
+
+# In pseudo-'LMS'-ish modal
+# Enter username
+# Enter pw
+# Click "Sign In"
+
+# On pseudo-'LMS'-ish warning page
+# Click "CONTINUE TO *******"
+# Click "Course Catalog"
+# Click "P-R"
+# Click "LOAD MORE"
+# Click "LOAD MORE" again
+# Click "Resources (24)" in the "Programming - Python (Open Sesame)" block
